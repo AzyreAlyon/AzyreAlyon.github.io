7be9968ccf02e3c1e2539a5844b702addcfc385a
AzyreAlyon <azyre.brown@carvercollegiate.org>
2025-02-25 15:39:09 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,7 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "white"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -27,22 +27,35 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     //toggleGrid();
 
 
     // TODO 2 - Create Platforms
 
-
-
+    createPlatform(138, 660, 100, 20, "pink");
+    createPlatform(720, 635, 100, 20, "pink");
+    createPlatform(250, 600, 150, 20, "pink");
+    createPlatform(510, 410, 145, 50, "pink");
+    createPlatform(510, 650, 125, 20, "pink");
+    createPlatform(1000,300, 10, 320, "pink");
+    createPlatform(900, 500, 125, 20, "pink");
+    createPlatform(650, 200, 110, 20, "pink");
+    createPlatform(650, 500, 110, 20, "pink");
+    createPlatform(200, 300, 30, 10, "pink");
+    createPlatform(350,300, 70, 10, "pink");
+    createPlatform(100,300, 70, 10, "pink");
 
     // TODO 3 - Create Collectables
-
+    createCollectable("diamond", 1350, 50);
+    createCollectable("diamond", 200, 270, 0.5, 0.7)
 
 
     
     // TODO 4 - Create Cannons
-
-
+    createCannon("top", 500, 850);
+createCannon("right", 300, 800);
+createCannon("left", 300, 800);
+createCannon("bottom", 1200, 800);
     
     
     //////////////////////////////////
